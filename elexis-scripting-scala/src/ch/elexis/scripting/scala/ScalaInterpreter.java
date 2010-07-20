97d12cd8336fc486d3b53dfd46f912b7897211b9
Gerry (pxg) <weririch@elexis.ch>
2010-07-20 14:52:10 +0200

    initial import

---
+++ elexis-scripting-scala/src/ch/elexis/scripting/scala/ScalaInterpreter.java
@@ -0,0 +1,24 @@
+package ch.elexis.scripting.scala;
+
+import ch.elexis.ElexisException;
+import ch.elexis.scripting.Interpreter;
+
+public class ScalaInterpreter implements Interpreter {
+
+	public ScalaInterpreter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setValue(String name, Object value) throws ElexisException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object run(String script, boolean showErrors) throws ElexisException {
+		return null;
+
+	}
+
+}
